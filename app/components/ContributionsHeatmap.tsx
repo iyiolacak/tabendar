e4dec6aac884457a381447f02e08f3f5caf342be
iyiolacak <iyiolacak0@gmail.com>
2025-02-10 20:50:53 +0300

    feat(ui): integrate SWR for data fetching in ContributionsHeatmap and add fetcher utility

--- app/components/ContributionsHeatmap.tsx
+++ app/components/ContributionsHeatmap.tsx
@@ -1,35 +1,25 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import useSWR from "swr";
 import HeatmapSquare from "./HeatmapSquare";
+import { fetcher } from "@/lib/utils";
 
 const WEEKS = 52;
 const DAYS_PER_WEEK = 7;
 
+const url = `https://www.github.com/api/${localStorage.getItem(
+  "githubUsername"
+)}`;
+
 const ContributionsHeatmap: React.FC = () => {
-  const CACHE_KEY = "heatmapDataCache";
-  const CACHE_TTL = 24 * 60 * 60 * 1000; // 24 hours
-  const [data, setData] = useState<number[][] | null>(null);
-
-  const loadData = async () => {
-    const cached = localStorage.getItem(CACHE_KEY);
-    if (cached) {
-      const { timeStamp, data } = JSON.parse(cached);
-      if (Date.now() - timestamp < CACHE_TTL) {
-        setData(cached);
-        // optionally refresh data background
-      }
-      return;
-    }
-
-    useEffect(() => {}, [fetchData]);
-  };
-
+  const { data, error } = useSWR(url, fetcher);
+  console.log(data ?? error)
   return (
     <div className="flex items-center justify-center">
       <div className="rounded-xl solid-dark-square p-4">
         <div className="flex min-h-full gap-2">
           {(
-            mockData ??
+            data ??
             Array.from({ length: WEEKS }, () => Array(DAYS_PER_WEEK).fill(99))
           ).map((week, weekIndex) => (
             <div key={weekIndex} className="flex flex-col gap-2">
@@ -37,7 +27,7 @@ const ContributionsHeatmap: React.FC = () => {
                 <HeatmapSquare
                   key={`${weekIndex}-${dayIndex}`}
                   level={level}
-                  isLoading={heatmapData === null} // Pass loading state correctly
+                  isLoading={data === null} // Pass loading state correctly
                 />
               ))}
             </div>
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const fetcher = async (...args: Parameters<typeof fetch>) => {
+  try {
+    const res = await fetch(...args);
+    if(!res.ok) {
+      throw new Error(`HTTP Error! Status: ${res.status}`)
+    }
+    return res.json();
+  } catch (err) {
+    console.error(err);
+  }
+
+}
