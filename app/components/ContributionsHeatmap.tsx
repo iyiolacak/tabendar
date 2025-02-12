1c7b75a01012238fc05e28fbc5b2427d25bb6c99
iyiolacak <iyiolacak0@gmail.com>
2025-02-12 19:17:29 +0300

    feat(ui): enhance ContributionsHeatmap with loading state and improved error handling

--- app/components/ContributionsHeatmap.tsx
+++ app/components/ContributionsHeatmap.tsx
@@ -6,7 +6,16 @@ import { fetcher } from "@/lib/utils";
 
 const WEEKS = 52;
 const DAYS_PER_WEEK = 7;
+type Contributions = {
+  date: string; // YYYY-MM-DD
+  count: number;
+  level: number;
+}
+interface Data {
+  total: Record<string, number>
+  contributions: Contributions
 
+}
 const ContributionsHeatmap: React.FC = () => {
   const [username, setUsername] = useState<string | null>(null);
 
@@ -18,47 +27,43 @@ const ContributionsHeatmap: React.FC = () => {
   const url = username
     ? `https://github-contributions-api.jogruber.de/v4/${username}`
     : null;
-    console.log("url is: ", url)
+  console.log("url is: ", url);
 
-  const { data, error } = useSWR(url, fetcher);
+  const { data, isLoading, error } = useSWR<Data>(url, fetcher);
   console.log(username);
   console.log("your damn data is ", data);
   if (error) {
     console.error("Error fetching data:", error);
   }
 
-  if(username === null ) {
+  if (username === null) {
     return (
-      <div className="p-64 bg-red-600">
-        Enter a github username please.
-        </div>
-    )
+      <div className="p-64 bg-red-600">Enter a github username please.</div>
+    );
   }
-  if(error) {
+  if (isLoading) {
     return (
-      <div className="p-64 bg-yellow-500">
-        An unexpected error occured.
-      </div>
-    )
+      <div className="p-64 bg-gray-100">Loading.</div>
+    );
+  }
+  if (error) {
+    return (
+      <div className="p-64 bg-yellow-500">An unexpected error occured.</div>
+    );
   }
   return (
     <div className="flex items-center justify-center">
       <div className="rounded-xl solid-dark-square p-4">
         <div className="flex min-h-full gap-2">
-          {(
-            Array.from({ length: WEEKS }, () => Array(DAYS_PER_WEEK).fill(99))
-          ).map((week, weekIndex) => (
-            <div key={`${weekIndex}`} className="flex flex-col gap-2">
-              {week.map((level, dayIndex) => (
+          {data !== undefined ? <div className="p-12 bg-blue-700">data.contributions</div> : "not loaded"}
+{/*             <div key={`${weekIndex}`} className="flex flex-col gap-2">
                 <HeatmapSquare
                   key={`${weekIndex}-${dayIndex}`}
                   level={level}
                   isLoading={data === null}
                 />
-              ))}
             </div>
-          ))}{" "}
-        </div>
+ */}        </div>
       </div>
     </div>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -5,13 +5,13 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const fetcher = async (...args: Parameters<typeof fetch>) => {
+export const fetcher = async <T>(...args: Parameters<typeof fetch>): Promise<T | undefined> => {
   try {
     const res = await fetch(...args);
     if(!res.ok) {
       throw new Error(`HTTP Error! Status: ${res.status}`)
     }
-    return res.json();
+    return res.json() as Promise<T>;
   } catch (err) {
     console.error(err);
   }
